d8af0321e7aa8e111b96ad9f6608db591bf0b29b
Dada Liu <hopluy@gmail.com>
2021-05-05 00:30:29 +0800

    6th commit

--- supplier_color_mapping_util.py
+++ supplier_color_mapping_util.py
@@ -12,12 +12,16 @@ def generate_structured_color_table():
     color_names = df["color_name"]
     
     new_columns = {"主要顏色": list(),
-                   "輔色": list(),
+                   "顏色-1": list(),
+                   "顏色-2": list(),
+                   "顏色-3": list(),
                    '條': list(), 
                    '邊': list(), 
                    '花': list(),
-                   '格': list()}
-    features = ['條', '邊', '花', '格']
+                   '格': list(),
+                   "印花": list(),
+                   "方格": list()}
+    features = ['條', '邊', "方格", "印花", '格', '花']
     
     for color_name in color_names:
         # 先判斷「是否具有」特殊條紋、花樣
@@ -26,9 +30,11 @@ def generate_structured_color_table():
                 new_columns[feature].append(1)
             else:
                 new_columns[feature].append(0)
-    
+
         # 再判斷該 (原始)顏色名稱 的顏色
         for feature in features:
+            if feature=='花' and "麻花" in color_name:
+                continue
             color_name = color_name.replace(feature, '')
         """
         if '底' in color_name:
@@ -37,11 +43,13 @@ def generate_structured_color_table():
         """
         if '底' in color_name:  # 有 "輔色"
             tmp_idx = color_name.index('底')
-            new_columns["主要顏色"].append(color_name[:tmp_idx])
-            new_columns["輔色"].append(color_name[tmp_idx+1:])
+            new_columns["顏色-1"].append(color_name[:tmp_idx])
+            new_columns["顏色-2"].append(color_name[tmp_idx+1:])
+            new_columns["顏色-3"].append("X")
         else:  # 無 "輔色"
-            new_columns["主要顏色"].append(color_name)
-            new_columns["輔色"].append("X")
+            new_columns["顏色-1"].append(color_name)
+            new_columns["顏色-2"].append("X")
+            new_columns["顏色-3"].append("X")
     
     #print(new_columns)
     df_out = pd.DataFrame.from_dict(new_columns)
@@ -59,48 +67,66 @@ def manually_classify_colors():
     color_table_path = "D:/MyPrograms/Clothes2U/DB/供應商資訊 DB/Lativ_product_info/color_table_v2_2.csv"
     
     df = pd.read_csv(color_table_path)
-    main_colors, sub_colors = df["主要顏色"], df["輔色"]
-    colors = set(main_colors.append(sub_colors))
+    main_colors, sub_colors_1, sub_colors_2 = df["主要顏色"], df["輔色-1"], df["輔色-2"]
+    tmp = main_colors.append(sub_colors_1)
+    colors = set(tmp.append(sub_colors_2))
     
     manual_def_colors = {"紅": ["玫紅","深玫紅","磚紅",
-                               "亮紅","橘紅","紅色",
+                               "亮紅","紅色","深紅",
                                "酒紅","深酒紅","桃紅",
-                               "暗紅","珊瑚紅"],
+                               "暗紅","珊瑚紅","暮紅",
+                               "紅"],
                          "橙": ["粉橘","桔","淺桔",
                                "磚桔","暗桔","桔色",
-                               "亮桔","深桔"],
+                               "亮桔","深桔","橘",
+                               "麻花桔"],
                          "黃": ["鵝黃","淡黃","黃",
                                "芥黃","粉黃","黃色",
                                "淺黃","麻花黃","暖黃",
-                               "亮黃"],
+                               "亮黃","復古黃"],
                          "粉": ["淺粉","桃粉","粉色",
                                "粉","淡粉","亮粉",
                                "嫩粉","淺粉紅","粉紅",
                                "深粉","深粉紅","亮桃粉",
-                               "麻花粉","藕粉","麻花粉桔"],
-                         "青": ["藏青","深藏青"],
-                         "藍": ["粉藍","天藍","湖綠藍",
+                               "麻花粉","藕粉","珊瑚粉"],
+                         "青": ["藏青","深藏青","麻花藏青"],
+                         "藍": ["天藍","湖綠藍","麻花藍",
                                "淺藍","藍","礦藍",
                                "丹寧藍","亮藍","藍色",
                                "湖藍","深藍","復古藍",
                                "淺麻花藍","水藍","寶藍",
-                               "深麻花藍","淡藍","麻花淺藍"],
+                               "深麻花藍","淡藍","麻花淺藍",
+                               "牛仔藍","麻花深藍"],
                          "紫": ["深紫","麻花紫","紫色",
-                               "粉紫","淺紫","淡紫"],
+                               "淺紫","淡紫","玫紫",
+                               "紫"],
                          "綠": ["綠色","軍綠","水綠",
                                "淺綠","橄欖綠","湖綠",
                                "湖水綠","亮綠","暗綠",
                                "深綠","淺水綠","嫩綠",
-                               "墨綠","淺湖綠","深軍綠"],
+                               "墨綠","淺湖綠","深軍綠",
+                               "藏青綠","麻花綠","草綠",
+                               "綠"],
                          "灰": ["黑麻灰","深灰色",
                                "深麻灰","深灰","灰色",
                                "暗灰","卡其灰","鐵灰",
-                               "黑灰","麻灰","淺灰"],
+                               "黑灰","麻灰","淺灰",
+                               "麻花灰","淺麻灰","灰"],
                          "黑": ["黑","黑色","麻花黑"],
                          "白": ["白","米杏","白色",
-                               "米色","麻灰白"],
+                               "米色","麻灰白","白色02",
+                               "米白","麻花米","淺杏",
+                               "杏色","白色01","米"],
                          "咖啡": ["咖啡","深咖","淺咖",
-                                 "麻花咖"]
+                                 "麻花咖","棕色","褐色",
+                                 "棕","淺棕"],
+                         "多色組": ["多色組","多色組01","多色組02",
+                                  "多色組03","多色組04","多色組05",
+                                  "多色組1","多色組2","多色組3"],
+                         "卡其": ["卡其","淺卡其","深卡其",
+                                "麻花卡其"],
+                         "膚色": ["膚色","淺膚"],
+                         "其它": ["X", "駝色", "彩"]
                          }
     print(manual_def_colors, '\n')
     n=0; c=0
@@ -157,8 +183,8 @@ def complete_color_icons():
             ans = input(f"請將 {color_name} 的顏色icon 放置到以下路徑:\n{color_icon_path}\n(press 'q' to exit): ")
             if ans == 'q':
                 break
-    
+
 if __name__ == "__main__":
-    #generate_structured_color_table()
-    #manually_classify_colors()
+    generate_structured_color_table()
+    manually_classify_colors()
     complete_color_icons()
